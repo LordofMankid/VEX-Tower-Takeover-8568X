f6e527df3d089e8807c2b5d099b4df01ce568fdc
Clarence Yeh <jiahengyeh@gmail.com>
2020-01-12 20:01:20 -0500

    Tied PID to a structure for passing in kP, kI, kD as one package

--- include/numberCrunching/PID.hpp
+++ include/numberCrunching/PID.hpp
@@ -1,5 +1,24 @@
 #include "main.h"
 
+typedef struct kPID{
+  double kP;
+  double kI;
+  double kD;
+} kPID;
 
+    //HELPER FUNCTIONS
+
+/*
+Sets values for a kPID structure.
+
+Paramaters:
+Structure of type kPID to write to.
+  This allows for setting a different kPID for different subsystems
+Target kP, kI, kD for this structure.
+
+*/
+kPID createkPID(kPID kPID, double kP, double kI, double kD);
+
+int PIDloop(kPID kPID, double units, double EncoderValue);
 
 int PIDloop(float Kp, float Ki, float Kd, double units, double EncoderValue);
--- src/numberCrunching/PID.cpp
+++ src/numberCrunching/PID.cpp
@@ -6,9 +6,48 @@ float derivative;
 float prevError = 0;
 const int integralMax = 25;
 
+kPID testPID;
 ////////
 
-//This PID loop returns just a number value - the value that the voltage needs to be. This is so that you can use this PIDloop function in a number of different usages.
+
+kPID createkPID(kPID kPID, double kP, double kI, double kD){
+  //struct kPID Kpid; <-- for optimization might wanna create a local variable to create kPID
+
+  kPID.kP = kP;
+  kPID.kI = kI;
+  kPID.kD = kD;
+
+  return kPID;
+}
+
+
+int PIDloop(kPID kPID, double units, double EncoderValue){
+  int voltage;
+
+  //sets error
+  error = units - EncoderValue;
+  pros::lcd::print(0, "error: %f, %f, %f", error, units, EncoderValue);
+  //increases error based on time taken to reach target - if resistance is encountered then integral will increase
+  integral = integral + error;
+  //resets integral if place is reached
+  if(error == 0)
+    integral = 0;
+  //reset integral if it becomes super big
+  if(fabs(error) >= integralMax)
+    integral = 0;
+  derivative = prevError - error; //finds derivative of errors
+  prevError = error; //updates prevError
+  //set voltage to equal the error
+  voltage = (error*kPID.kI) + (integral*kPID.kI) + (derivative*kPID.kD);
+  if(voltage > 127)
+    voltage = 127;
+  if(voltage < -127)
+    voltage = -127;
+
+  return voltage;
+}
+
+
 int PIDloop(float Kp, float Ki, float Kd, double units, double EncoderValue){
   int voltage;
 

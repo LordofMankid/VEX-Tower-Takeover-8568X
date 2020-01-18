9a3466a2e8b3b537311b64322c1b6fed49ec513e
Clarence Yeh <jiahengyeh@gmail.com>
2020-01-18 14:39:09 -0500

    odometry testing + tuning

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -22,9 +22,9 @@ pros::ADIEncoder trackingRight('G','H');
 //SETUP VARIABLE
 
 const double RADIUS = 10;
-const double RIGHTWHEELDISTANCE = 4.875;
-const double LEFTWHEELDISTANCE = 4.875;
-const double XWHEELDISTANCE = 4.875;
+const double RIGHTWHEELDISTANCE = 4.5;
+const double LEFTWHEELDISTANCE = 4.5;
+const double XWHEELDISTANCE = 3.5;
 const double TR_TICK_INCH = (2.75*PI)/TICKS_TRACKING_REV;
 const double TR_INCH_TICK = 360/(2.75*PI);
 const double TICK_DEG_REG = DEG_CIRCLE/TICKS_REGULAR_REV;

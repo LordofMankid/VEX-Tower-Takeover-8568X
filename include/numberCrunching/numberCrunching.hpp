4a787649620bb9d528477591cf02cb890d0d524f
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2020-01-26 21:15:13 -0500

    Added vector summation functions

--- include/numberCrunching/numberCrunching.hpp
+++ include/numberCrunching/numberCrunching.hpp
@@ -5,15 +5,20 @@ extern double angle;
 extern int stepChangeTimes;
 //HELPER FUNCTIONS
 
-//CALCULATION FUNCTIONS
 void nextStep(int stepSet, int time);
+
+//CALCULATION FUNCTIONS
+
+rectCoord vectorSummation(rectCoord v1, rectCoord v2);
+
+rectCoord vectorSummation(rectCoord v1, position v2);
 
 double findDistance(rectCoord targetCoord, position position);
 
 double findDistance(position position1, position position2);
 
-int positionReachCheck(position currentPosition, position lastPosition, int driveStopParameter, rectCoord target);
 
+int positionReachCheck(position currentPosition, position lastPosition, int driveStopParameter, rectCoord target);
 double positionReachCheck(double currentPositionY, double currentPositionX, double lastPositionY, double lastPositionX,  double driveStopParameter, double targetY, double targetX);
 /*
 Returns a driveStopParameter that is used to exit a PID loop. Checks if there has been a position change, and if there is, then driveStopParameter is reset

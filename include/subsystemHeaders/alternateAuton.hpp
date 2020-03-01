3505e63a190aecff447651006131e9479bb8244a
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2020-03-01 10:07:56 -0500

    Moved okapi BACK to 4.0.3

--- include/subsystemHeaders/alternateAuton.hpp
+++ include/subsystemHeaders/alternateAuton.hpp
@@ -3,7 +3,7 @@
 //CONSTANTS
 extern const double REG_INCH_TICK;
 
-extern okapi::ChassisControllerIntegrated myChassis;
-extern okapi::AsyncMotionProfileController profileController;
+//extern okapi::ChassisControllerIntegrated myChassis;
+//extern okapi::AsyncMotionProfileController profileController;
 //extern okapi::ChassisControllerBuilder myChassis;
 void timeDrive(int time, int voltage);
